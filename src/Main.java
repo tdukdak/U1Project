ac1341d53ef6c447a6122f07da21f0e41cc57a59
BT_4N2_12 <tdukdak9287@bths.edu>
2022-09-21 11:50:56 -0400

    program asked for bill total and it is defined as a double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Please enter the bill total: ");
+        double bill = s.nextDouble();
+
     }
 }
