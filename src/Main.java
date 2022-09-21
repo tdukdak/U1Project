03fe4abca8c34ee82d0a85c7b53b7feeb13fba6a
BT_4N2_12 <tdukdak9287@bths.edu>
2022-09-21 12:17:34 -0400

    Added other questions such as tip and party size and started to calculate values.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,19 @@ public class Main {
 
         System.out.print("Please enter the bill total: ");
         double bill = s.nextDouble();
+        System.out.println(" ");
+
+        System.out.print("Please enter the tip percentage you wish for: ");
+        double tipPercent = s.nextDouble();
+        System.out.println(" ");
+
+        System.out.print("how many people are in your party: ");
+        int partySize = s.nextInt();
+        System.out.println(" ");
+
+        double tipTotal =  (tipPercent);
+
+
 
     }
 }
