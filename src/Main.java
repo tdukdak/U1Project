770a1d55778b1ecf98930c88744ed33a134ed23d
tdukdak <tdukdak9287@bths.edu>
2022-09-24 20:10:56 -0400

    I completed the mathematical operations and the display portion of the project as well as commented it.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //Lines 8-20 ask the user for input for values such as the
+        //total and tip percentage any monetary based values such as the bill are declared as doubles
+        // while party size is declared an integer as you cannot have a decimal point
+        // in number of people in a party
         Scanner s = new Scanner(System.in);
 
         System.out.print("Please enter the bill total: ");
@@ -15,7 +19,22 @@ public class Main {
         int partySize = s.nextInt();
         System.out.println(" ");
 
-        double tipTotal =  (tipPercent);
+        //Lines 26-29 compile the information and run operations that calculate certain values
+        //such as total tip which is ran through an operation that includes multiplication and division
+        // the Math.round function
+
+        double tipTotal = Math.round(((tipPercent / 100) * bill) * 100.0) / 100.0;
+        double totalBill = (tipTotal + bill);
+        double tipPerPerson = Math.round((tipTotal / partySize) * 100.0) / 100.0;
+        double totalPerPerson = Math.round((totalBill / partySize) * 100.0) / 100.0;
+
+        //lines 33-36 the values calculated previously are displayed using print statements.
+
+        System.out.println("total tip: " + tipTotal);
+        System.out.println("total bill: " + totalBill);
+        System.out.println("tip per person: " + tipPerPerson);
+        System.out.println("total per person: " + totalPerPerson);
+
 
 
 
